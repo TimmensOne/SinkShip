ce63e5a33ce8a0149fa7e0f06988bbe9f0efe94c
Timon Lorenz <TimmensOne@users.noreply.github.com>
2019-11-04 16:06:32 +0100

    Print Board

--- SinkShip.c
+++ SinkShip.c
@@ -1,6 +1,45 @@
+#include <stdio.h>
+
+int board[10][10] = {{0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0},
+                    {0,0,0,0,0,0,0,0,0,0}};
+
+const char rowTitles[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+const char colTitles[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'};
 void setBoard()
 {
-    
+    printf("\n    %c   %c   %c   %c   %c   %c   %c   %c   %c   %c", colTitles[0], colTitles[1], colTitles[2], colTitles[3], colTitles[4], colTitles[5], colTitles[6], colTitles[7], colTitles[8], colTitles[9]);
+
+    printf("\n  -----------------------------------------\n");
+    for (int row = 0; row < 10; row++)
+    {
+        printf("%c ", rowTitles[row]);
+        printf("|");
+        for (int col = 0; col < 10; col++)
+        {
+            if (board[row][col] == 0)
+            {
+                printf("   ");
+            }
+            else if (board[row][col] == 1)
+            {
+                //printf(" %c ", player1Type);
+            }
+            else
+            {
+                //printf(" %c ", player2Type);
+            }
+            printf("|");
+        }
+    printf("\n  -----------------------------------------\n");
+}
 }
 int main ()
 {
